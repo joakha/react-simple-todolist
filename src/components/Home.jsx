ffa1300f714f938a263274961f83626d86169022
joakha <72503684+joakha@users.noreply.github.com>
2024-03-27 12:17:14 +0200

    implement appbar to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,29 @@
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 const Home = () => {
 
     return (
 
         <>
-        
-        <h1>Welcome to my app!</h1>
-        
+            <Container maxWidth="xl">
+                <CssBaseline />
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6">
+                            Simple Todolist
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <h1>Welcome to my app!</h1>
+            </Container>
+
         </>
-    
-    
+
+
     )
 
 }
